b6425b8e870dbe9a61ee09558ec0ba57ad8d5d98
EC2 Default User <ec2-user@ip-172-31-90-29.ec2.internal>
2022-01-06 07:02:37 +0000

    [Finish]3-2

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,17 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @blog = Blog
+  end
+  
+  def create
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/blogs_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class BlogsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get blogs_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get blogs_show_url
+    assert_response :success
+  end
+
+  test "should get view" do
+    get blogs_view_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get blogs_edit_url
+    assert_response :success
+  end
+
+end
